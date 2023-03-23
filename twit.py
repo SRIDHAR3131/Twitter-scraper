b14b241c958647dd5b51f945105495c75ea99e81
SRIDHAR R <68391060+SRIDHAR3131@users.noreply.github.com>
2023-03-23 10:11:09 +0530

    feat

--- twit.py
+++ twit.py
@@ -106,10 +106,10 @@ if col1.button('submit'):
 #------------------------------------------Connect to the MongoDB server--------------------------------------
     cl1, cl2, cl3= col2.columns(3,gap='small')
 
-    cl3.button('Upload MongoDB')
+    cl1.button('Upload MongoDB')
     #connecting database through atlas in MongoDB
     
-    client =MongoClient("mongodb+srv://sridhar15:HeyramSridhar@cluster0.gkifau6.mongodb.net/?retryWrites=true&w=majority")
+    client =MongoClient("mongodb+srv://<credentials>@cluster0.gkifau6.mongodb.net/test")
 
     #if database is exists use this fololwing commend get_databaase to access the database
     db = client.get_database("Twitter")
@@ -126,7 +126,7 @@ if col1.button('submit'):
     def convert_df(T_df ):
         return T_df.to_csv().encode('utf-8')
     csv = convert_df(T_df)
-    cl1.download_button(
+    cl2.download_button(
                             label="Download CSV file ",
                             data=csv,
                             file_name='user_data.csv',
@@ -137,7 +137,7 @@ if col1.button('submit'):
     def convert_df(T_df ):
         return T_df.to_json().encode('utf-8')
     json = convert_df(T_df)
-    cl2.download_button(
+    cl3.download_button(
                             label="Download JSON file",
                             data=json,
                             file_name='user_data.json',
